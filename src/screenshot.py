8b2b9ea111513b1af41d12753ef1dcd19fa7010c
connorakey <connorakey@proton.me>
2025-07-29 21:02:08 +1000

    Added functionality capture_questions_screenshot and capture_answers_screenshot functions
    Added functionality to said functions by saving the regions as a variable (definined in config.yaml) created a screenshot object with pyautogui then created a filename variable for the filename of the screenshot, then saved the screenshot then returned the file name for future development in other file that will use the file name, the file name is a combination of a 12 letter string pretty much ensuring randomness. Added todo list aswell for future things to be developed on the file

--- src/screenshot.py
+++ src/screenshot.py
@@ -4,19 +4,52 @@
 # It captures screenshots of the question and answers regions based on the configuration settings.
 # This file stores the screenshots in workspace/screenshots and deletes old screenshots if necessary.
 
+# TODO:
+# Add debug functionality to this script, possibily with an external python script like debug.py
+# Implement deletion of old screenshot folders
+# Check if file exists, so errors don't occur
+
 from pathlib import Path
 import pyautogui
 from config import config
+import random
+import string
+
+def random_letter_string(num: int):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(num))
 
 workspace = Path("workspace")
 screenshots = workspace / "screenshots"
 
+questions_dir = screenshots / "questions"
+answers_dir = screenshots / "answers"
 
-def capture_question_screenshot(filename: str):
+random_letters = 12
+
+questions_dir.mkdir(parents=True, exist_ok=True)
+answers_dir.mkdir(parents=True, exist_ok=True)
+
+
+def capture_question_screenshot():
     question_region = config["ui"]["question_region"]
+    screenshot = pyautogui.screenshot(region=question_region)
 
-    print("Question region is", question_region)
+    filename = f"mathsonline-question-{random_letter_string(random_letters)}.png"
+
+    save_path = questions_dir / filename
+    screenshot.save(save_path)
+
+    return filename
 
 
-def capture_answers_screenshot(filename: str):
-    return
+def capture_answers_screenshot():
+    answer_region = config["ui"]["answers_region"]
+    screenshot = pyautogui.screenshot(region=answer_region)
+
+    filename = f"mathsonline-answer-{random_letter_string(random_letters)}.png"
+
+    save_path = answers_dir / filename
+    screenshot.save(save_path)
+    
+    return filename
